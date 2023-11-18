962042a13673df98463bbda1f3bf46fb9cac7d8e
tsyhahaha <tsy1433701769@163.com>
2023-11-18 15:17:28 +0800

    add inference code

---
+++ inference.sh
@@ -0,0 +1,5 @@
+python giws/CFIT/scripts/inference.py  \
+	--model_path /home/taosiyuan/Giws/output/clip_wt_attn/checkpoints/best_checkpoint.ckpt \
+	--img_file /home/taosiyuan/Giws/data/0001.jpg \
+	--text_file /home/taosiyuan/Giws/data/0001.txt \
+
