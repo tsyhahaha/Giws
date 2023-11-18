ec1f1bfcfbc5820112c8795fadaf270c73255713
Tao Siyuan <tsy1433701769@163.com>
2023-11-18 15:49:55 +0800

    add param to select model mode:  clip or single(bert, vit)

--- inference.sh
+++ inference.sh
@@ -1,5 +1,6 @@
 python giws/CFIT/scripts/inference.py  \
 	--model_path /home/taosiyuan/Giws/output/clip_wt_attn/checkpoints/best_checkpoint.ckpt \
+	--name clip \
 	--img_file /home/taosiyuan/Giws/data/0001.jpg \
 	--text_file /home/taosiyuan/Giws/data/0001.txt \
 
